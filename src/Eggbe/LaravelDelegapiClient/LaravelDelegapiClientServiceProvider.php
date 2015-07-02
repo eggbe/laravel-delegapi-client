91e6d5d4285b25d1fbbf9c4977169a1d66d05640
hacpaka <hacpaka@eggbe.com>
2015-07-02 14:51:34 +0300

    +

---
+++ src/Eggbe/LaravelDelegapiClient/LaravelDelegapiClientServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace Eggbe\LaravelDelegapiClient;
+
+use \Illuminate\Support\ServiceProvider;
+use \Illuminate\Support\Facades\Config;
+
+use \Eggbe\DelegapiClient\Client;
+
+class LaravelDelegapiClientServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register bindings in the container.
+	 * @return void
+	 */
+	public function register() {
+		$this->package('eggbe/laravel-delegapi-client');
+		LaravelDelegapiClient::$Config = Config::get('laravel-delegapi-client::config');
+	}
+
+}
---
+++ src/config/config.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	'url' => null,
+	'hash'=> null,
+];
